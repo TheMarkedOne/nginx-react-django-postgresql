07438b6f120d114cbfeb34d160c75e32f3e4b1c1
TheMarkedOne <132334856+TheMarkedOne@users.noreply.github.com>
2023-05-15 18:30:46 +0400

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "# nginx-react-django-postgresql" >> README.md
-git init
-git add README.md
-git add --all .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://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@github.com/TheMarkedOne/nginx-react-django-postgresql.git
-git push -u origin main
